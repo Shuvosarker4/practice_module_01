17637ec5e6d7f4e40bae9e38765c390e7a7a8cad
Shuvosarker4 <shuvo01797451@gmail.com>
2024-04-01 22:21:57 +0600

    solution number 4

---
+++ solution_04.c
@@ -0,0 +1,20 @@
+/**
+ *  You need to take one integer value as input
+ *  and tell if the value is positive or
+ *  negative or zero.
+ **/
+
+#include <stdio.h>
+int main(){
+    int number;
+    printf("Enter a number: ");
+    scanf("%d",&number);
+
+    if(number>0){
+        printf("Positive");
+    }else if(number==0){
+        printf("Zero");
+    }else{
+        printf("Negative");
+    }
+}
